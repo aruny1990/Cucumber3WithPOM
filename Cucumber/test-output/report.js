3a8de72824a9f509b2179e5852f3042af2ccf083
unknown <33333+aruny1990@users.noreply.github.com>
2024-07-21 09:42:36 +0530

    commiting the code

---
+++ Cucumber/test-output/report.js
@@ -0,0 +1,149 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("GoogleSearch.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Google Search",
+  "description": "",
+  "id": "google-search",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 11410816000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Searching Google Page",
+  "description": "",
+  "id": "google-search;searching-google-page",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@Search"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "I Launch Goolge",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I Search For Selenium",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I Select The Link",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "I Search Something On Selenium Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "GoogleSearchSteps.i_Launch_Goolge()"
+});
+formatter.result({
+  "duration": 5133904700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GoogleSearchSteps.i_Search_For_Selenium()"
+});
+formatter.result({
+  "duration": 4822311900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GoogleSearchSteps.i_Select_The_Link()"
+});
+formatter.result({
+  "duration": 3680962700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GoogleSearchSteps.i_Search_Something_On_Selenium_Page()"
+});
+formatter.result({
+  "duration": 4460010000,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 3446865300,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 7468201800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "Searching Google Page",
+  "description": "",
+  "id": "google-search;searching-google-page",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 9,
+      "name": "@Search"
+    }
+  ]
+});
+formatter.step({
+  "line": 11,
+  "name": "I Launch Goolge",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "I Search For Selenium",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "I Select The Link",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 14,
+  "name": "I Search Something On Selenium Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "GoogleSearchSteps.i_Launch_Goolge()"
+});
+formatter.result({
+  "duration": 4020463400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GoogleSearchSteps.i_Search_For_Selenium()"
+});
+formatter.result({
+  "duration": 4457726900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GoogleSearchSteps.i_Select_The_Link()"
+});
+formatter.result({
+  "duration": 3368187700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GoogleSearchSteps.i_Search_Something_On_Selenium_Page()"
+});
+formatter.result({
+  "duration": 4732636400,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 193238500,
+  "status": "passed"
+});
+});
---
+++ README.md
@@ -0,0 +1,13 @@
+# Cucumber-POM
+
+##### *Clone the project Directory*
+
+### Test Examples
+```
+mvn clean install
+```
+> Features
+> - Hooks
+> - Extent Report
+> - Page Object Model
+> - Attached Failed Screenshots 
